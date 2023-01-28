f8b5ec56f29efd994a0e4d263987be9a339997e2
PieMenard <pinedo.m.gustavo@gmail.com>
2023-01-28 09:20:24 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let result = string;
+    if (num < 0)
+        return 'ERROR';
+    if (num == 0)
+        return '';
+    for (let c = 1; c < num; c++)
+    {
+        result = result + string;
+    }
+    return result;
 };
 
 // Do not edit below this line
