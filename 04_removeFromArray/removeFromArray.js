da3d52079754dc8e57566427e3b1fb46389ba57c
PieMenard <pinedo.m.gustavo@gmail.com>
2023-01-28 11:47:32 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) 
+{
+    const newArray = [];
+
+    //this function fills 'newArray' with contents from 'array'
+    //but only with elements that are not on 'args' 
+    function checkAndPush(item)
+    {
+        if (!args.includes(item))
+           newArray.push(item);
+    }
+
+    array.forEach(element => checkAndPush(element));
+    
+    return newArray;
 
 };
 
