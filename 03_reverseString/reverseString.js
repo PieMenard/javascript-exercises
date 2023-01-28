70d5cac89ab4bac2208f5951c7d83ce9572abbdc
PieMenard <pinedo.m.gustavo@gmail.com>
2023-01-28 09:31:29 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let c = string.length - 1; c >= 0; c--)
+    {
+        reversedString += string[c];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
