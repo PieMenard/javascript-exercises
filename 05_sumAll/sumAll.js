21c818bb2d8346ef0013a642cf3148236a43b807
PieMenard <pinedo.m.gustavo@gmail.com>
2023-01-30 09:36:05 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+   
+    let sum = 0;
 
+    if ( x<0 || y<0)
+        return 'ERROR';
+    if ( (typeof x != 'number')||(typeof y != 'number'))
+        return 'ERROR';
+
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for (let i = x; i <= y ; i++ ) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
