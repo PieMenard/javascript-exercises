4090a622763f3925a7f604fa5b604b7fdef94177
PieMenard <pinedo.m.gustavo@gmail.com>
2023-02-13 14:04:36 -0400

    Complete TOP 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let cleanWord = word.toLowerCase().replace(/[^a-z]/g, "");
+    const reversedWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reversedWord;
 };
 
 // Do not edit below this line
