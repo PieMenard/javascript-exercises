b05af6a03cb982a2256a886c837de9d4d9b020b8
PieMenard <pinedo.m.gustavo@gmail.com>
2023-02-13 14:20:20 -0400

    complete TOP 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    //generate fibonacci array
+    if (x < 0 )
+        return "OOPS";
+    let fibo = [0,1];
+    for (let c=2;c<=x;c++)
+        fibo[c]=fibo[c-2] + fibo[c-1];
+    return fibo[x];
 };
 
 // Do not edit below this line
