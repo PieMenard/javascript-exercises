6dbbc79d1b03d72e4ff303680598261838ee77f3
PieMenard <pinedo.m.gustavo@gmail.com>
2023-02-13 13:31:36 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, a) => total + a,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product,a) => product*a,1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	/*let result = 1;
+  for (c=0; c < y;c++)
+    result *= x;
+    return result;*/
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+  for (let c = 1; c <= x;c++)
+    result *= c;
+  return result;
 };
 
 // Do not edit below this line
